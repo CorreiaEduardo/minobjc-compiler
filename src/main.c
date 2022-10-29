2b0619ec1fb484af709aea523bfd8db026929ea3
Eduardo Correia <correiaeduardojr@gmail.com>
2022-10-29 11:09:46 -0300

    refactor: move input file to constant

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include "include/analex.h"
 
+static const char INPUT_FILE[] = "../examples/analex.moc";
+
 int main(int argc, char const *argv[])
 {
   FILE *fd;
   Token token;
 
-  if ((fd = fopen("../examples/analex.moc", "r")) == NULL)
+  if ((fd = fopen(INPUT_FILE, "r")) == NULL)
     error("Não foi possivel ler o código fonte.");
 
   while (token.type != EOP)
